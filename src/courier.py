30031810197ca052f267311f87d88a47f540125d
unknown <orthogonaltrajectory@gmail.com>
2020-04-24 14:15:57 -0800

    Log integration with primary library

--- src/courier.py
+++ src/courier.py
@@ -1,8 +1,9 @@
 import paho.mqtt.client as mqtt
+import courier_logger as CourierLogger
 import time
 
 class Courier:
-    def __init__(self, name, ip, port):
+    def __init__(self, name, ip, port, logpath=None):
         self.name = name
         self.ip = ip
         self.port = port
@@ -10,9 +11,13 @@ class Courier:
         self.client.connected_flag = False
         self.topics = {}
 
+        self.logger = CourierLogger(path=logpath)
+        self.logger.info("Courier instance name: {}".format(self.name))
+
     def subscribe(self):
         formatted_topics = []
         for topic in self.topics.keys():
+            self.logger.info("Subscribed to topic: {}".format(topic))
             formatted_topics.append((topic, 0))
         self.client.subscribe(formatted_topics)
 
@@ -20,6 +25,7 @@ class Courier:
         def on_message_callback(client, userdata, msg):
             if msg != None:
                 payload = msg.payload.decode("utf-8")
+                self.logger.info("Topic: {} received the payload: {}".format(msg.topic, payload))
                 callback = self.topics[msg.topic]
                 callback(payload)
         return on_message_callback
@@ -27,34 +33,44 @@ class Courier:
     def configured_on_connect_callback(self):
         def on_connect_callback(client, userdata, flags, retCode):
             if retCode == 0:
-                print ("Host connection successful! Return code: {}".format(retCode))
+                good_msg = "Host connection successful! Return code: {}".format(retCode)
+                print (good_msg)
+                self.logger.info(good_msg)
                 self.client.connected_flag = True
                 self.subscribe()
             else:
-                print ("Host connection failed! Return code: {}".format(retCode))
+                err_msg = "Host connection failed! Return code: {}".format(retCode)
+                print (err_msg)
+                self.logger.error(err_msg)
         return on_connect_callback
-
 
     def establish_connection(self):
         try:
+            self.logger.info("Attempting to establish connection to {}:{}".format(self.ip, self.port))
             self.client.on_connect = self.configured_on_connect_callback()
             self.client.on_message = self.configured_on_message_callback()
             self.client.connect(self.ip, port=self.port)
         except Exception as e:
             print(e)
+            self.logger.error("Exception occured MQTT connection - disconnecting courier client...")
             self.client.disconnect()
 
     def add_topic(self, topic, callback):
         self.topics[topic] = callback
+        self.logger.info("Added topic: {}".format(topic))
 
     def run(self):
         self.establish_connection()
         try:
             self.client.loop_forever()
             while not self.client.connected_flag:
-                print("Waiting to connect to host...")
+                pending_msg = "Waiting to connect to host..."
+                print(pending_msg)
+                self.logger.warning(pending_msg)
                 time.sleep(1)
         except Exception as e:
-            print("Disconnecting from host with exception: {}".format(e))
+            err_msg = "Disconnecting from host with exception: {}".format(e)
+            print(err_msg)
+            self.logger.error(err_msg)
             self.client.loop_stop()
             self.client.disconnect()
